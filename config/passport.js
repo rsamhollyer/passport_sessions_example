9cc8063470fdbe5d9ff896efcd462c365bef29b1
Sam Hollyer <rsamhollyer@gmail.com>
2021-07-17 08:58:13 -0500

    quick fixes

--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,7 @@
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const connection = require('./database');
-const User = connection.models.User;
+
+const { User } = connection.models;
 
 // TODO: passport.use();
